ede12988ba09300fd3b0ceb4fbaab2b2f46a1157
Techboterp <75273975+techboterp2020@users.noreply.github.com>
2023-07-07 15:55:47 +0400

    3cx

---
+++ 3cxcrm/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "3CX CRM lookup",
+
+    'summary': """
+        lookup for incoming call from 3cx to odoo""",
+
+    
+
+    "images": ["static/description/banner.png"],
+    'author': "FL1 sro",
+    'website': "https://fl1.cz",
+
+    # Categories can be used to filter modules in modules listing
+    
+    # for the full list
+    'category': 'Productivity',
+    'version': '15.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','crm', 'partner_firstname'],
+    'license' : 'AGPL-3',
+   
+
+    # always loaded
+    'data': [
+     
+        'data/data.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    
+}
